a1c02a525c327663c8642ed5215f8332bd8ccc8f
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-20 16:22:33 +0700

    feat: konfigurasi awal aplikasi SIRUPI

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'role' => \App\Http\Middleware\CheckRole::class,
+            'unit_kerja' => \App\Http\Middleware\CheckUnitKerja::class,
+            'log.activity' => \App\Http\Middleware\LogActivity::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
